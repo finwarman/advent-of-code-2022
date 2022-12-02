0eb61b38f45a7491e716095a756099274a705c5f
Fin <finleywarman@hotmail.co.uk>
2022-12-02 08:32:21 +0000

    Solve part 1

--- 02/01.py
+++ 02/01.py
@@ -12,8 +12,41 @@ rows = [row.strip() for row in data.split('\n')[:-1]]
 
 total = 0
 
-for row in rows:
-    if row:
-        total += int(row)
+# 1 rock
+# 2 paper
+# 3 scissors
 
-print(total)
+# 0 loss
+# 3 draw
+# 6 won
+
+shapes = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+}
+
+translate = {
+    'X': 'A',
+    'Y': 'B',
+    'Z': 'C'
+}
+
+beats = {
+    'A': 'C',
+    'B': 'A',
+    'C': 'B'
+}
+
+score = 0
+for row in rows:
+    move, counter = row.split()
+    score += shapes[counter]
+    counter_move = translate[counter]
+
+    if move == counter_move:
+        score += 3
+        continue
+    if beats[counter_move] == move:
+        score += 6
+print(score)
