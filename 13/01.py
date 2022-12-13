371390b51dc42f9abfe466642b9458b269c5f7ce
Fin <finleywarman@hotmail.co.uk>
2022-12-13 12:14:35 +0000

    Refactor, remove debug output

--- 13/01.py
+++ 13/01.py
@@ -14,50 +14,29 @@ pairs = [[row.strip() for row in m.strip().split('\n')] for m in data.split('\n\
 
 pairs = [[ast.literal_eval(line) for line in pair] for pair in pairs]
 
-def compare(lhs, rhs, indent=''):
-    print(f"{indent}- Compare {lhs} vs {rhs}")
-    indent = '  ' + indent
-
+def compare(lhs, rhs):
     if isinstance(lhs, list) and isinstance(rhs, int):
-        rhs = [rhs]
-        print(f"{indent}- Mixed types; convert right to {rhs} and retry comparison")
-        return compare(lhs, rhs, indent)
-    elif isinstance(lhs, int) and isinstance(rhs, list):
-        lhs = [lhs]
-        print(f"{indent}- Mixed types; convert left to {lhs} and retry comparison")
-        return compare(lhs, rhs, indent)
+        return compare(lhs, [rhs])
+    if isinstance(lhs, int) and isinstance(rhs, list):
+        return compare([lhs], rhs)
 
-    for i in range(max(len(lhs), len(rhs))):
-        if i >= len(lhs):
-            print(f"{indent}- Left side ran out of items, so inputs are in the right order")
-            return True
-        elif i >= len(rhs):
-            print(f"{indent}- Right side ran out of items, so inputs are not in the right order")
-            return False
+    for i in range(min(len(lhs), len(rhs))):
         l, r = lhs[i], rhs[i]
+        if isinstance(l, list) or isinstance(r, list):
+            c = compare(l, r)
+            if c is not None:
+                return c
+        elif l != r:
+            return l < r
 
-        if isinstance(l, int) and isinstance(r, int):
-            print(f"{indent}- Compare {l} vs {r}")
-            if l < r:
-                print(f"  {indent}- Left side is smaller, so inputs are in the right order")
-                return True
-            if l > r:
-                print(f"  {indent}- Right side is smaller, so inputs are not in the right order")
-                return False
-        else:
-            result = compare(l, r, indent)
-            if result is not None:
-                return result
-
-    return None
-
+    if len(lhs) == len(rhs):
+        return None
+    return len(lhs) < len(rhs)
 
 result = 0
 for i, (lhs, rhs) in enumerate(pairs):
-    print(f"== Pair {i+1} ==")
     if (compare(lhs, rhs)):
         result += i+1
-    print()
 
 print(result)
 
