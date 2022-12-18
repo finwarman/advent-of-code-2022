41e6523c60822b90febeeaf2a6911c6e9bb4b891
Fin <finleywarman@hotmail.co.uk>
2022-12-18 20:34:05 +0000

    Add day 15 part 2 solutions

--- 15/02.py
+++ 15/02.py
@@ -28,13 +28,13 @@ for row in rows:
     sensors[s] = b
 
 
-# MIN, MAX = 0, 4000000
-MIN, MAX = 0, 20
+MIN, MAX = 0, 4000000
+# MIN, MAX = 0, 20
 
 def manhattan(point1, point2):
     return abs(point1[0]-point2[0]) + abs(point1[1]-point2[1])
 
-points = set()
+ranges = {}
 count = 0
 for sensor, beacon in sensors.items():
     dist = manhattan(sensor, beacon)
@@ -47,30 +47,43 @@ for sensor, beacon in sensors.items():
         n = dist+1 - abs(row - y)
         width = (2*n-1) # nth odd number
 
-        # TODO: convert to ranges
-
         start = x - (width//2)
-        for i in range(max(MIN, start), min(start+width, MAX+1)):
-            p = (i, row)
-            points.add(p)
+        rng = (max(MIN, start), min(start+width, MAX+1)-1)
 
-print()
-result = 0
-for x in range(MIN, MAX+1):
-    for y in range(MIN, MAX+1):
-        if (x,y) not in points:
-            print(f"point: {x,y}")
-            result = (4000000*x)+y
-            break
-    else:
+        if row not in ranges:
+            ranges[row] = set()
+        ranges[row].add(rng)
+
+# simplify ranges (merge intervals)
+point = None
+for y in range(MIN, MAX+1):
+    intervals = [list(r) for r in sorted(ranges[y])]
+
+    stack = []
+    stack.append(intervals[0])
+    for i in intervals[1:]:
+        # merge overlapping intervals
+        if stack[-1][0] <= i[0] <= stack[-1][-1]:
+            stack[-1][-1] = max(stack[-1][-1], i[-1])
+        else:
+            stack.append(i)
+
+    if len(stack) == 1:
         continue
-    break
 
+    if stack[0][1]+1 < stack[1][1]:
+        point = (stack[0][1]+1, y)
+        break
+
+x, y = point
+result = (4000000*x) + y
+
+print(f"point: {x,y}")
 print()
 print(result)
 
-# output: 0
-# assert result == 0
-
 # demo:
-assert result == 56000011
+# assert result == 56000011
+
+# output: 13213086906101
+assert result == 13213086906101
