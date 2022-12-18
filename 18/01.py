8554fad5b77c3c8618ac2e5fd17ef2ed30301016
Fin <finleywarman@hotmail.co.uk>
2022-12-18 20:55:13 +0000

    Add day 18 part 1 solution

---
+++ 18/01.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+# ==== INPUT ====
+
+INPUT = 'input.txt'
+with open(INPUT, 'r', encoding='UTF-8') as file:
+    data = file.read()
+
+rows = [row.strip() for row in data.split('\n')[:-1]]
+
+# ==== SOLUTION ====
+
+c = []
+for row in rows:
+    x, y, z = [int(i) for i in row.split(',')]
+    c.append((x,y,z))
+
+cubes = set(c)
+
+faces = 0
+for cube in c:
+    x, y, z = cube
+    exposed = 6
+
+    for dx, dy, dz in [
+        (-1, 0, 0),( 1, 0, 0),
+        ( 0,-1, 0),( 0, 1, 0),
+        ( 0, 0,-1),( 0, 0, 1),
+    ]:
+        new = (x+dx, y+dy, z+dz)
+        if new != cube and new in cubes:
+            exposed -= 1
+
+    faces += exposed
+
+print(faces)
+
+# output: 3390
+assert faces == 3390
